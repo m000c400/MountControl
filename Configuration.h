8f871bd3a6ea5007206db77f1a51daaf25a3e722
Mike Roberts <m.roberts777@btinternet.com>
2014-01-26 18:38:06 +0000

    Creation of Configuration Class

---
+++ Configuration.h
@@ -0,0 +1,27 @@
+#ifndef CONFIGURATION_h
+#define CONFIGURATION_h
+
+#include <stdlib.h>
+#if ARDUINO >= 100
+#include <Arduino.h>
+#else
+#include <WProgram.h>
+#include <wiring.h>
+#endif
+
+// These defs cause trouble on some versions of Arduino
+#undef round
+
+
+/////////////////////////////////////////////////////////////////////
+class Configuration : 
+{
+	public:
+	protected:
+				
+	private:
+};
+
+
+#endif 
+
